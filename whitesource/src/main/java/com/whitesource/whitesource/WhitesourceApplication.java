d285253338077878bee6a9cd0ed8ea59a271027c
ashugc2023 <ashugc2023@gmail.com>
2023-04-19 13:58:15 -0400

    white source test

---
+++ whitesource/src/main/java/com/whitesource/whitesource/WhitesourceApplication.java
@@ -0,0 +1,13 @@
+package com.whitesource.whitesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitesourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitesourceApplication.class, args);
+	}
+
+}
---
+++ whitesource/src/test/java/com/whitesource/whitesource/WhitesourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitesource.whitesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitesourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
